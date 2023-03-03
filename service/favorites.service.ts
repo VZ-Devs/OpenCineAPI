4efe4e791c61c3ee8b2c0edae5f8d750611ddd04
rfrias8 <rfrias8@gmail.com>
2023-03-03 17:25:09 -0600

    added favorites.service file

---
+++ service/favorites.service.ts
@@ -0,0 +1,27 @@
+import { FavoritesEngineRepository } from "../repository/favoritesEngine.repository";
+
+export class FavoritesEngineService {
+
+    private FavoritesEngineRepository: FavoritesEngineRepository;
+
+    constructor() {
+        this.FavoritesEngineRepository = new FavoritesEngineRepository();
+    }
+
+    // async getTasks() {
+    //     return await this.FavoritesEngineRepository.getTasks();
+    // }
+
+    async createFavorite(favorite) {
+        return await this.FavoritesEngineRepository.createFavorite(favorite);
+    }
+
+    // async updateTask(favorite) {
+    //     return await this.FavoritesEngineRepository.updateTask(favorite);
+    // }
+
+    // async deleteTask(taskId) {
+    //     return await this.FavoritesEngineRepository.deleteTask(taskId);
+    // }
+
+}
