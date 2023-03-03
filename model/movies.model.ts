645c7ca8f33795541d8424359930ee0601263999
rfrias8 <rfrias8@gmail.com>
2023-03-03 17:23:07 -0600

    created favorites, user, and movies models

---
+++ model/movies.model.ts
@@ -0,0 +1,21 @@
+import { Table, Column, Model, BelongsToMany } from 'sequelize-typescript'
+import { Users } from './user.model';
+import { Favorites } from './favorites.model';
+
+
+@Table
+export class Movies extends Model {
+  
+  @Column
+  user_id: string
+
+  @Column
+  url: string
+
+  @Column
+  title: string
+
+  @BelongsToMany(() => Users, () => Favorites)
+  users: Users[];
+
+}
